736062745a569a919dd5e2d9c96d71b9745631c9
shockValue666 <mapedeveloper@protonmail.com>
2024-05-22 04:34:37 -0700

    more

--- src/components/address.tsx
+++ src/components/address.tsx
@@ -6,7 +6,7 @@ const Address = () => {
     <div className='w-full flex items-center justify-center my-8'>
         <Link href="https://pump.fun/HhnpF8jEeZT9Zfw4EefacR4RrXbc59Tu747LwtaJwwJY" target='_blank'>
             <p className='text-[30px] bold'>
-                HhnpF8jEeZT9Zfw4EefacR4RrXbc59Tu747LwtaJwwJY
+                So11111111111111111111111111111111111111112
             </p>
         </Link>
     </div>
