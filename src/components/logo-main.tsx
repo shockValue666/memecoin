232b0d1ad18719202a9adcc40e8b19a11699d73d
shockValue666 <mapedeveloper@protonmail.com>
2024-05-22 04:33:12 -0700

    more

--- src/components/logo-main.tsx
+++ src/components/logo-main.tsx
@@ -3,11 +3,12 @@ import Image from "next/image";
 // import logo from '../../public/logo.jpg'
 import tremp from "../../public/tremp.png"
 import real from '../../public/real.png'
+import tremp1 from '../../public/Tremp_1.png'
 
 const LogoMain = () => {
   return (
     <div className='w-full flex items-center justify-center'>
-        <Image width={300} height={300} src={real} alt='logo'>
+        <Image width={300} height={300} src={tremp1} alt='logo'>
 
         </Image>
     </div>
